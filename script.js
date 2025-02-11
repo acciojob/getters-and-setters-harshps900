1d5ca0fc8063473dd7881e90cf0ea07de9cd14ae
harshps900 <151460810+harshps900@users.noreply.github.com>
2025-02-11 12:32:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,51 +1,29 @@
-// Person Class
 class Person {
   constructor(name, age) {
-    this._name = name; // Private property for name
-    this._age = age; // Private property for age
-  }
-
-  // Getter for name
-  get name() {
-    return this._name;
-  }
-
-  // Setter for age
-  set age(age) {
-    this._age = age; // Setter for age, allows age to be updated
+    this.name = name;
+    this.age = age;  // Directly public access
   }
 
   // Getter for age
-  get age() {
-    return this._age; // Getter for age
+  getAge() {
+    return this.age;
+  }
+
+  // Setter for age
+  setAge(value) {
+    if (value >= 0) {
+      this.age = value;
+    } else {
+      console.log("Age cannot be negative.");
+    }
+  }
+
+  getName() {
+    return this.name;
   }
 }
 
-// Student Class that inherits from Person
-class Student extends Person {
-  constructor(name, age) {
-    super(name, age); // Call the parent class constructor
-  }
-
-  // Method to log that the student is studying
-  study() {
-    console.log(`${this.name} is studying`);
-  }
-}
-
-// Teacher Class that inherits from Person
-class Teacher extends Person {
-  constructor(name, age) {
-    super(name, age); // Call the parent class constructor
-  }
-
-  // Method to log that the teacher is teaching
-  teach() {
-    console.log(`${this.name} is teaching`);
-  }
-}
-
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+const person = new Person("John", 25);
+console.log(person.age); // 25 (direct access)
+person.age = 30;         // Directly set age
+console.log(person.age); // 30
