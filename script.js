e93d641ed96b0fab73fe2fea0fe86760acbe65e4
harshps900 <151460810+harshps900@users.noreply.github.com>
2025-02-11 12:15:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
-//complete this code
-class Person {}
+// Person Class
+class Person {
+  constructor(name, age) {
+    this._name = name; // Private property for name
+    this._age = age; // Private property for age
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(age) {
+    this._age = age; // Setter for age, allows age to be updated
+  }
+
+  // Getter for age
+  get age() {
+    return this._age; // Getter for age
+  }
+}
+
+// Student Class that inherits from Person
+class Student extends Person {
+  constructor(name, age) {
+    super(name, age); // Call the parent class constructor
+  }
+
+  // Method to log that the student is studying
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Teacher Class that inherits from Person
+class Teacher extends Person {
+  constructor(name, age) {
+    super(name, age); // Call the parent class constructor
+  }
+
+  // Method to log that the teacher is teaching
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
